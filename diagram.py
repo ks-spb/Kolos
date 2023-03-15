29c87c1130f64a5b17e5239314005eaf75b95bca
ks_spb <ksv@kiktoys.ru>
2023-03-15 14:28:01 +0300

    Расширил наименование узлов time, чтобы было проще читать диаграммы

--- diagram.py
+++ diagram.py
@@ -24,11 +24,12 @@ name 'хоро'             - серый
 
 class Point:
     """ Точки """
-    def __init__(self, id, name, type):
+    def __init__(self, id, name, type, name_2):
 
         self.id = id
         self.name = name
         self.type = type
+        self.name_2 = name_2
 
         if name == 'time' or name == 'time_p':
             # Временные точки входящих сигналов
@@ -59,25 +60,27 @@ class Point:
             # Вывод
             self.group = 'out'
             color = 'mediumorchid'
-
+        #
         else:
             # Входящие
             self.group = 'in'
             color = 'cadetblue'
 
         with Cluster(self.group):
-            self.node = Node(f'{id} {name}', style='filled', fillcolor=color, fontsize='20pt')
+            self.node = Node(f'{id} {name} {name_2}', style='filled', fillcolor=color, fontsize='20pt')
 
 
 # Подключение к БД
 with contextlib.closing(sqlite3.connect('Li_db_v1_4.db')) as conn:
 
-    nodes = conn.execute("SELECT ID, name, type FROM tochki")
+    # 15.03.23 - добавлено name2
+    nodes = conn.execute("SELECT ID, name, type, name2 FROM tochki")
     connections = conn.execute("SELECT id_start, id_finish FROM svyazi WHERE id > 2")
 
-    with Diagram('My Diagram', direction='LR'): # LR или TB
+    with Diagram('My Diagram', direction='TB'): # LR или TB
 
-        points = [Point('0', 'time', '')]  # Все точки
+        points = [Point('0', 'time', '', "")]  # Все точки
+
 
         for i in nodes.fetchall():
             points.append(Point(*i))
@@ -85,3 +88,72 @@ with contextlib.closing(sqlite3.connect('Li_db_v1_4.db')) as conn:
 
         for one, two in connections.fetchall():
             points[one].node >> points[two].node
+
+
+
+
+def new_diagram():
+    class Point:
+        """ Точки """
+
+        def __init__(self, id, name, type, name_2):
+
+            self.id = id
+            self.name = name
+            self.type = type
+            self.name_2 = name_2
+
+            if name == 'time' or name == 'time_p':
+                # Временные точки входящих сигналов
+                self.group = 't'
+                color = 'lemonchiffon'
+
+            elif name == 'time_0' or name == 't0':
+                # Временные точки
+                self.group = 't0'
+                color = 'limegreen'
+
+            elif name == 'time_0' or name == 't0':
+                # Действия
+                self.group = 'action'
+                color = 'aqua'
+
+            elif name == 'хоро':
+                # Реакция Хорошо
+                self.group = 'reaction'
+                color = 'salmon'
+
+            elif name == 'пло':
+                # Реакция Плохо
+                self.group = 'reaction'
+                color = 'silver'
+
+            elif type == 'print':
+                # Вывод
+                self.group = 'out'
+                color = 'mediumorchid'
+            #
+            else:
+                # Входящие
+                self.group = 'in'
+                color = 'cadetblue'
+
+            with Cluster(self.group):
+                self.node = Node(f'{id} {name} {name_2}', style='filled', fillcolor=color, fontsize='20pt')
+
+    # Подключение к БД
+    with contextlib.closing(sqlite3.connect('Li_db_v1_4.db')) as conn:
+
+        # 15.03.23 - добавлено name2
+        nodes = conn.execute("SELECT ID, name, type, name2 FROM tochki")
+        connections = conn.execute("SELECT id_start, id_finish FROM svyazi WHERE id > 2")
+
+        with Diagram('My Diagram', direction='TB'):  # LR или TB
+
+            points = [Point('0', 'time', '', "")]  # Все точки
+
+            for i in nodes.fetchall():
+                points.append(Point(*i))
+
+            for one, two in connections.fetchall():
+                points[one].node >> points[two].node
