ff31f4124c28b0ea9ab0c3755337a6e732e30bc2
ks-spb <ksv@kiktoys.ru>
2024-03-07 16:37:45 +0300

    Сохранение перед изменением записи в один файл

--- test.py
+++ test.py
@@ -13,16 +13,7 @@ screenshot = pyautogui.screenshot()
 # Уменьшаем разрешение изображения до 192x108 пикселей
 resized_image = screenshot.resize((192, 108), Image.LANCZOS)
 
-# Создаем новые массивы для цветов white, yellow и red
-white_pixels = []
-yellow_pixels = []
-red_pixels = []
-violet_pixels = []
-light_blue_pixels = []
-green_pixels = []
-blue_pixels = []
-gray_pixels = []
-black_pixels = []
+resized_image.save('resized_image.png')
 
 
 # Создаем новый Excel-файл
@@ -36,6 +27,8 @@ ws_green = wb.create_sheet('green')
 ws_blue = wb.create_sheet('blue')
 ws_gray = wb.create_sheet('gray')
 ws_black = wb.create_sheet('black')
+ws_all = wb.create_sheet('all')
+
 
 
 # Получаем RGB цифры каждого пикселя и записываем в соответствующий массив и Excel-файл
@@ -43,9 +36,10 @@ for y in range(resized_image.height):
     for x in range(resized_image.width):
         r, g, b = resized_image.getpixel((x, y))
         rgb_str = f"{r},{g},{b}"
+        ws_all.cell(row=y + 1, column=x + 1, value=rgb_str)
 
         if r >= 188 and g >= 188 and b >= 188:
-            ws_white.cell(row=y + 1, column=x + 1, value=999)
+            ws_white.cell(row=y + 1, column=x + 1, value=1)
             ws_yellow.cell(row=y + 1, column=x + 1, value=0)
             ws_red.cell(row=y + 1, column=x + 1, value=0)
             ws_violet.cell(row=y + 1, column=x + 1, value=0)
@@ -55,9 +49,9 @@ for y in range(resized_image.height):
             ws_gray.cell(row=y + 1, column=x + 1, value=0)
             ws_black.cell(row=y + 1, column=x + 1, value=0)
         elif r >= 188 and g >= 188 and b < 188:
-            # yellow_pixels.append(999)
+            # yellow_pixels.append(1)
             ws_white.cell(row=y + 1, column=x + 1, value=0)
-            ws_yellow.cell(row=y + 1, column=x + 1, value=999)
+            ws_yellow.cell(row=y + 1, column=x + 1, value=1)
             ws_red.cell(row=y + 1, column=x + 1, value=0)
             ws_violet.cell(row=y + 1, column=x + 1, value=0)
             ws_light_blue.cell(row=y + 1, column=x + 1, value=0)
@@ -68,7 +62,7 @@ for y in range(resized_image.height):
         elif r >= 188 and g < 188 and b >= 188:
             ws_white.cell(row=y + 1, column=x + 1, value=0)
             ws_yellow.cell(row=y + 1, column=x + 1, value=0)
-            ws_red.cell(row=y + 1, column=x + 1, value=999)
+            ws_red.cell(row=y + 1, column=x + 1, value=1)
             ws_violet.cell(row=y + 1, column=x + 1, value=0)
             ws_light_blue.cell(row=y + 1, column=x + 1, value=0)
             ws_green.cell(row=y + 1, column=x + 1, value=0)
@@ -79,7 +73,7 @@ for y in range(resized_image.height):
             ws_white.cell(row=y + 1, column=x + 1, value=0)
             ws_yellow.cell(row=y + 1, column=x + 1, value=0)
             ws_red.cell(row=y + 1, column=x + 1, value=0)
-            ws_violet.cell(row=y + 1, column=x + 1, value=999)
+            ws_violet.cell(row=y + 1, column=x + 1, value=1)
             ws_light_blue.cell(row=y + 1, column=x + 1, value=0)
             ws_green.cell(row=y + 1, column=x + 1, value=0)
             ws_blue.cell(row=y + 1, column=x + 1, value=0)
@@ -90,18 +84,18 @@ for y in range(resized_image.height):
             ws_yellow.cell(row=y + 1, column=x + 1, value=0)
             ws_red.cell(row=y + 1, column=x + 1, value=0)
             ws_violet.cell(row=y + 1, column=x + 1, value=0)
-            ws_light_blue.cell(row=y + 1, column=x + 1, value=999)
+            ws_light_blue.cell(row=y + 1, column=x + 1, value=1)
             ws_green.cell(row=y + 1, column=x + 1, value=0)
             ws_blue.cell(row=y + 1, column=x + 1, value=0)
             ws_gray.cell(row=y + 1, column=x + 1, value=0)
             ws_black.cell(row=y + 1, column=x + 1, value=0)
-        elif r < 188 and g >= 188 and b < 188:
+        elif r < 188 and g >= 122 and b < 188:
             ws_white.cell(row=y + 1, column=x + 1, value=0)
             ws_yellow.cell(row=y + 1, column=x + 1, value=0)
             ws_red.cell(row=y + 1, column=x + 1, value=0)
             ws_violet.cell(row=y + 1, column=x + 1, value=0)
             ws_light_blue.cell(row=y + 1, column=x + 1, value=0)
-            ws_green.cell(row=y + 1, column=x + 1, value=999)
+            ws_green.cell(row=y + 1, column=x + 1, value=1)
             ws_blue.cell(row=y + 1, column=x + 1, value=0)
             ws_gray.cell(row=y + 1, column=x + 1, value=0)
             ws_black.cell(row=y + 1, column=x + 1, value=0)
@@ -112,7 +106,7 @@ for y in range(resized_image.height):
             ws_violet.cell(row=y + 1, column=x + 1, value=0)
             ws_light_blue.cell(row=y + 1, column=x + 1, value=0)
             ws_green.cell(row=y + 1, column=x + 1, value=0)
-            ws_blue.cell(row=y + 1, column=x + 1, value=999)
+            ws_blue.cell(row=y + 1, column=x + 1, value=1)
             ws_gray.cell(row=y + 1, column=x + 1, value=0)
             ws_black.cell(row=y + 1, column=x + 1, value=0)
         elif r < 188 and g < 188 and (b >= 56 and b < 188):
@@ -123,7 +117,7 @@ for y in range(resized_image.height):
             ws_light_blue.cell(row=y + 1, column=x + 1, value=0)
             ws_green.cell(row=y + 1, column=x + 1, value=0)
             ws_blue.cell(row=y + 1, column=x + 1, value=0)
-            ws_gray.cell(row=y + 1, column=x + 1, value=999)
+            ws_gray.cell(row=y + 1, column=x + 1, value=1)
             ws_black.cell(row=y + 1, column=x + 1, value=0)
         elif r < 188 and g < 188 and b < 56:
             ws_white.cell(row=y + 1, column=x + 1, value=0)
@@ -134,10 +128,20 @@ for y in range(resized_image.height):
             ws_green.cell(row=y + 1, column=x + 1, value=0)
             ws_blue.cell(row=y + 1, column=x + 1, value=0)
             ws_gray.cell(row=y + 1, column=x + 1, value=0)
-            ws_black.cell(row=y + 1, column=x + 1, value=999)
+            ws_black.cell(row=y + 1, column=x + 1, value=1)
 
 # Сохраняем Excel-файл
 wb.save('pixels_colors.xlsx')
 
+# Открываем созданный Excel-файл
+wb = openpyxl.load_workbook('pixels_colors.xlsx')
 
+# Изменяем ширину столбцов в каждом листе до значения 3 (26 пикселей)
+for sheet in wb.sheetnames:
+    ws = wb[sheet]
+    for column in ws.columns:
+        ws.column_dimensions[column[0].column_letter].width = 3
+
+# Сохраняем изменения
+wb.save('pixels_colors.xlsx')
 
